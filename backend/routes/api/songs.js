fdeb11beeb4ba30f51968efd1abd39b8e9f3b5a5
Kelly <kelly.a.artola@ricealumni.net>
2022-07-28 21:42:17 -0500

    get song by songId without artist

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -1,8 +1,41 @@
 const express = require("express");
+const { UPSERT } = require("sequelize/types/query-types");
 
 const router = express.Router();
 
-const { Song } = require("../../db/models");
+const { Song, User, Album } = require("../../db/models");
+
+router.get("/:songId", async (req, res) => {
+  let { songId } = req.params;
+  songId = parseInt(songId);
+
+  const song = await Song.findByPk(songId);
+
+  if (!song) {
+    res.status(404);
+    return res.json({
+      message: "Song couldn't be found",
+      statusCode: 404,
+    });
+  }
+
+  const songs = await Song.findByPk(songId, {
+    include: [
+      {
+        model: User,
+        attributes: ["id", "username", ["imageUrl", "previewImage"]],
+      },
+      {
+        model: Album,
+        attributes: ["id", "title", ["imageUrl", "previewImage"]],
+      },
+    ],
+  });
+
+  return res.json({
+    songs,
+  });
+});
 
 router.get("/", async (req, res) => {
   let Songs = await Song.findAll({
