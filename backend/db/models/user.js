534216414e214aa803de112283b6ae9753f98961
Kelly <kelly.a.artola@ricealumni.net>
2022-07-30 01:00:09 -0500

    output username

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     toSafeObject() {
       const { id, firstName, lastName, username, email } = this; // context will be the User instance
-      return { id, firstName, lastName, email };
+      return { id, firstName, lastName, email, username };
     }
     validatePassword(password) {
       return bcrypt.compareSync(password, this.password.toString());
