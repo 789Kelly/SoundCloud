2efe197cf0dab4b64cc78519408d7538b4374e83
Kelly <kelly.a.artola@ricealumni.net>
2022-07-29 21:30:25 -0500

    edit playlist

--- backend/routes/api/playlists.js
+++ backend/routes/api/playlists.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { Playlist, Song } = require("../../db/models");
+const { Album, Playlist, Song } = require("../../db/models");
 
 const { requireAuth } = require("../../utils/auth");
 const { check } = require("express-validator");
@@ -59,6 +59,50 @@ router.get("/:playlistId", async (req, res) => {
   });
 
   return res.json(playlists);
+});
+
+router.put("/:playlistId", requireAuth, validatePlaylist, async (req, res) => {
+  const { user } = req;
+  let { name, imageUrl } = req.body;
+  let { playlistId } = req.params;
+
+  const playlist = await Album.findByPk(playlistId);
+
+  if (!playlist) {
+    res.status(404);
+    return res.json({
+      message: "Playlist couldn't be found",
+      statusCode: 404,
+    });
+  }
+
+  if (user.id === playlist.userId) {
+    playlist.update({
+      name,
+      imageUrl,
+    });
+
+    let id = playlist.id;
+    let userId = playlist.userId;
+    let createdAt = playlist.createdAt;
+    let updatedAt = playlist.updatedAt;
+    let previewImage = playlist.imageUrl;
+
+    return res.json({
+      id,
+      userId,
+      name,
+      createdAt,
+      updatedAt,
+      previewImage,
+    });
+  } else {
+    res.status(403);
+    return res.json({
+      message: "Forbidden",
+      statusCode: 403,
+    });
+  }
 });
 
 router.post("/", requireAuth, validatePlaylist, async (req, res) => {
