f142d9651a74d689e039b2d9f67ecaee21ebd676
Kelly <kelly.a.artola@ricealumni.net>
2022-09-04 13:05:33 -0500

    setPassword

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -35,6 +35,15 @@ function LoginForm() {
           required
         />
       </label>
+      <label>
+        Password
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
       <button type="submit">Log In</button>
     </form>
   );
