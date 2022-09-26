bc2ed87a47c6e06b5ea6603a1bb423a6ca7cacbe
Kelly <kelly.a.artola@ricealumni.net>
2022-09-26 00:16:28 -0500

    home page draft

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -32,6 +32,41 @@ function Home() {
           </NavLink>
         </div>
       </div> */}
+      <div className="centered-6">
+        <NavLink to="/albums">
+          <button className="btn">Explore trending albums</button>
+        </NavLink>
+      </div>
+      <div className="container-4">
+        <img
+          src="/images/devices.png"
+          alt="Devices"
+          style={{ maxWidth: "50%" }}
+        />
+        <div className="right-text">
+          <div className="right-1">Never stop listening</div>
+          <img src="/images/bar.jpeg" alt="Devices" className="right-3" />
+          <div className="right-2">
+            SoundCloud is available on Web, iOS, Android, Sonos, Chromecast, and
+            Xbox One.
+          </div>
+          <div className="right-4">
+            <a
+              href="https://apps.apple.com/us/app/soundcloud/id336353151"
+              target={"_blank"}
+            >
+              <button className="apple">
+                <img
+                  src="/images/apple.png"
+                  alt="Download on the App Store"
+                  height="100%"
+                  width="100%"
+                />
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
       <div className="container-5">
         <div className="left-text">
           <div className="left-1">Calling all creators</div>
@@ -39,11 +74,17 @@ function Home() {
             Get on Soundcloud to connect with fans, share your sounds, and grow
             your audience. What are you waiting for?
           </div>
+          <div className="left-3">
+            <NavLink to="/albums">
+              <button className="btn-4">Find out more</button>
+            </NavLink>
+          </div>
         </div>
         <img
-          src="/images/CharmaineH.jpeg"
+          src="/images/Charmaine.jpeg"
           alt="Charmaine"
-          style={{ maxWidth: "50%" }}
+          style={{ maxWidth: "66%" }}
+          className="image-1"
         />
       </div>
       <div className="container-6">
