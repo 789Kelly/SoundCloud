d4ee0ec1440c9ab6cfb1b0c6fa83726093e6cb75
Kelly <kelly.a.artola@ricealumni.net>
2022-10-04 22:34:29 -0500

    trying isLoaded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -55,7 +55,7 @@ function App() {
             <CreateSongForm />
           </Route>
           <Route exact path="/playlists">
-            <PlaylistsPage />
+            <PlaylistsPage isLoaded={isLoaded} />
           </Route>
           <Route exact path="/playlists/new">
             <CreatePlaylistForm />
--- frontend/src/components/PlaylistsPage/index.js
+++ frontend/src/components/PlaylistsPage/index.js
@@ -5,14 +5,15 @@ import { useEffect } from "react";
 import { Link, Redirect, NavLink } from "react-router-dom";
 import "./PlaylistsPage.css";
 
-const PlaylistsPage = () => {
+const PlaylistsPage = ({ isLoaded }) => {
   const dispatch = useDispatch();
 
   const playlists = Object.values(useSelector((state) => state.playlists));
   const sessionUser = useSelector((state) => state.session.user);
+  const [isLoaded, setIsLoaded] = useState(false);
   // if no session user, there shouldn't be any playlists so redirect to /; but why is it showing all playlists with user logged in
   useEffect(() => {
-    dispatch(fetchPlaylists());
+    dispatch(fetchPlaylists()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
   if (!sessionUser?.id) return <Redirect to="/" />;
@@ -26,37 +27,40 @@ const PlaylistsPage = () => {
   //   );
   // } else {
   return (
-    <>
-      <div id="gradient-boxs">
-        <div id="left-playlist">
-          <div>
-            <p id="album-titres">My Playlists</p>
-            <span id="album-descriptions">{sessionUser?.username}</span>
+    isLoaded && (
+      <>
+        <div id="gradient-boxs">
+          <div id="left-playlist">
+            <div>
+              <p id="album-titres">My Playlists</p>
+              <span id="album-descriptions">{sessionUser?.username}</span>
+            </div>
+            <span id="circles">MIXES</span>
           </div>
-          <span id="circles">MIXES</span>
+          <img
+            src="https://media.pitchfork.com/photos/5f08e1ae9f0d624cf3ecafc7/1:1/w_320,c_limit/legends%20never%20die_juice%20wrld.jpg"
+            alt="Best Playlists"
+            id="album-imgs"
+          />
         </div>
-        <img
-          src="https://media.pitchfork.com/photos/5f08e1ae9f0d624cf3ecafc7/1:1/w_320,c_limit/legends%20never%20die_juice%20wrld.jpg"
-          alt="Best Playlists"
-          id="album-imgs"
-        />
-      </div>
-      <div id="for-screens">
-        <NavLink to={`/playlists/new`}>
-          <button className="btn-3s">Add new playlist</button>
-        </NavLink>
-        <hr id="discover-hrs" />
-        <ul className="remove-indent">
-          {playlists?.map((playlist) => (
-            <li key={playlist.id}>
-              <PlaylistItem playlist={playlist} />
-              <hr id="discover-hrss" />
-            </li>
-          ))}
-        </ul>
-      </div>
-      {/* <Link to="/playlists/new">Add New Playlist</Link> */}
-    </>
+        <div id="for-screens">
+          <NavLink to={`/playlists/new`}>
+            <button className="btn-3s">Add new playlist</button>
+          </NavLink>
+          <hr id="discover-hrs" />
+          <ul className="remove-indent">
+            {isLoaded &&
+              playlists.map((playlist) => (
+                <li key={playlist.id}>
+                  <PlaylistItem playlist={playlist} />
+                  <hr id="discover-hrss" />
+                </li>
+              ))}
+          </ul>
+        </div>
+        {/* <Link to="/playlists/new">Add New Playlist</Link> */}
+      </>
+    )
   );
 };
 
