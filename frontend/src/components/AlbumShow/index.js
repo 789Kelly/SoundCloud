4dee61ceeb5dab89952bbe0ea50a7cfe24e99acb
Kelly <kelly.a.artola@ricealumni.net>
2023-03-04 23:57:24 -0600

    react-howler and seeders

--- frontend/src/components/AlbumShow/index.js
+++ frontend/src/components/AlbumShow/index.js
@@ -87,14 +87,14 @@ const AlbumShow = () => {
           ))}
         </ul>
       </div>
-      <iframe
+      {/* <iframe
         width="100%"
         height="166"
         scrolling="no"
         frameborder="no"
         allow="autoplay"
         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/293&amp;{ ADD YOUR PARAMETERS HERE }"
-      ></iframe>
+      ></iframe> */}
     </>
   );
 };
--- frontend/src/components/SongItem/index.js
+++ frontend/src/components/SongItem/index.js
@@ -1,9 +1,19 @@
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
-import ReactPlayer from "react-player";
+import ReactHowler from "react-howler";
+import { useState } from "react";
 
 function SongItem({ song }) {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
+  const [playing, setPlaying] = useState(false);
+
+  const play = () => {
+    setPlaying(true);
+  };
+
+  const pause = () => {
+    setPlaying(false);
+  };
 
   const user = useSelector((state) => state.session.user);
   return (
@@ -18,7 +28,14 @@ function SongItem({ song }) {
         </NavLink>
         <button className="btn-3ss">Delete</button>
       </div> */}
-      <ReactPlayer url={song.url} />
+      <div>
+        <ReactHowler
+          playing={playing}
+          src="https://res.cloudinary.com/diyjzl1ol/video/upload/v1677969351/01_-_What_s_Going_On_pnssve.mp3"
+        />
+        <button onClick={play}>></button>
+        <button onClick={pause}>||</button>
+      </div>
     </div>
   );
 }
